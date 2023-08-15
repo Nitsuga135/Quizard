f740790fb1d5f905dc7da5d4ec65f76fa3779836
Your Name <aig2352000@gmail.com>
2023-08-15 01:27:09 -0300

    Tercer commit subiendo readmi y cambios a la vista para js

---
+++ public/scripts/lolQuiz/modeControl.js
@@ -0,0 +1,22 @@
+let numeroJuego = 0;
+
+/*botones de menu principal del juego*/
+let modeAdivinaPj = document.getElementById("mode-adivina-pj");
+let modeFrases = document.getElementById("mode-frases");
+let modeHabilidades = document.getElementById("mode-habilidades");
+let modeFotos = document.getElementById("mode-fotos");
+
+/*Containers de los juegos*/
+let containerQuienEs = document.getElementById("quien-es");
+let containerFrases = document.getElementById("frases");
+let containerHabilidades = document.getElementById("habilidades");
+let containerFotos = document.getElementById("fotos");
+
+/*Botones*/
+
+//siguiente nivel
+let nextLv = document.querySelectorAll(".next-lv");
+
+//menu principal
+let menu = document.querySelectorAll("menu-principal");
+
